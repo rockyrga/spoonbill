c94daf248fe98c7a379310c51ebf082b347b875b
Rocky Chen <rocky.chen@rentguard.co.uk>
2015-12-31 16:21:11 +0000

    Added local date time assertion

--- src/main/java/com/rc/spoonbill/test/asserts/DateAssert.java
+++ src/main/java/com/rc/spoonbill/test/asserts/DateAssert.java
@@ -1,5 +1,6 @@
 package com.rc.spoonbill.test.asserts;
 
+import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -7,32 +8,48 @@ import org.junit.Assert;
 
 public class DateAssert {
 
-	public static void assertEqual(Date expected, Date actual) {
+    public static void assertEqual(Date expected, Date actual) {
 
-		Calendar expectedCalendar = Calendar.getInstance();
-		expectedCalendar.setTime(expected);
+        Calendar expectedCalendar = Calendar.getInstance();
+        expectedCalendar.setTime(expected);
 
-		Calendar actualCalendar = Calendar.getInstance();
-		actualCalendar.setTime(actual);
+        Calendar actualCalendar = Calendar.getInstance();
+        actualCalendar.setTime(actual);
 
-		Assert.assertEquals(expectedCalendar.get(Calendar.YEAR), actualCalendar.get(Calendar.YEAR));
-		Assert.assertEquals(expectedCalendar.get(Calendar.MONTH), actualCalendar.get(Calendar.MONTH));
-		Assert.assertEquals(expectedCalendar.get(Calendar.DATE), actualCalendar.get(Calendar.DATE));
-	}
+        Assert.assertEquals(expectedCalendar.get(Calendar.YEAR), actualCalendar.get(Calendar.YEAR));
+        Assert.assertEquals(expectedCalendar.get(Calendar.MONTH), actualCalendar.get(Calendar.MONTH));
+        Assert.assertEquals(expectedCalendar.get(Calendar.DATE), actualCalendar.get(Calendar.DATE));
+    }
 
-	public static void assertNotEqual(Date expected, Date actual) {
+    public static void assertNotEqual(Date expected, Date actual) {
 
-		Calendar expectedCalendar = Calendar.getInstance();
-		expectedCalendar.setTime(expected);
+        Calendar expectedCalendar = Calendar.getInstance();
+        expectedCalendar.setTime(expected);
 
-		Calendar actualCalendar = Calendar.getInstance();
-		actualCalendar.setTime(actual);
+        Calendar actualCalendar = Calendar.getInstance();
+        actualCalendar.setTime(actual);
 
-		if (expectedCalendar.get(Calendar.YEAR) == actualCalendar.get(Calendar.YEAR)
-				&& expectedCalendar.get(Calendar.MONTH) == actualCalendar.get(Calendar.MONTH)
-				&& expectedCalendar.get(Calendar.DATE) == actualCalendar.get(Calendar.DATE)) {
+        if (expectedCalendar.get(Calendar.YEAR) == actualCalendar.get(Calendar.YEAR)
+                && expectedCalendar.get(Calendar.MONTH) == actualCalendar.get(Calendar.MONTH)
+                && expectedCalendar.get(Calendar.DATE) == actualCalendar.get(Calendar.DATE)) {
 
-			Assert.fail("Both date are the same");
-		}
-	}
+            Assert.fail("Both date are the same");
+        }
+    }
+
+    public static void assertEqual(LocalDate expected, LocalDate actual) {
+
+        Assert.assertEquals(expected.getYear(), actual.getYear());
+        Assert.assertEquals(expected.getMonth(), actual.getMonth());
+        Assert.assertEquals(expected.getDayOfMonth(), actual.getDayOfMonth());
+    }
+
+    public static void assertNotEqual(LocalDate expected, LocalDate actual) {
+
+        if (expected.getYear() == actual.getYear() && expected.getMonth() == actual.getMonth()
+                && expected.getDayOfMonth() == actual.getDayOfMonth()) {
+
+            Assert.fail("Both date are the same");
+        }
+    }
 }
--- src/main/java/com/rc/spoonbill/test/asserts/DateTimeAssert.java
+++ src/main/java/com/rc/spoonbill/test/asserts/DateTimeAssert.java
@@ -1,5 +1,6 @@
 package com.rc.spoonbill.test.asserts;
 
+import java.time.LocalDateTime;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -7,19 +8,29 @@ import org.junit.Assert;
 
 public class DateTimeAssert {
 
-	public static void assertEqual(Date expected, Date actual) {
+    public static void assertEqual(Date expected, Date actual) {
 
-		Calendar expectedCalendar = Calendar.getInstance();
-		expectedCalendar.setTime(expected);
+        Calendar expectedCalendar = Calendar.getInstance();
+        expectedCalendar.setTime(expected);
 
-		Calendar actualCalendar = Calendar.getInstance();
-		actualCalendar.setTime(actual);
+        Calendar actualCalendar = Calendar.getInstance();
+        actualCalendar.setTime(actual);
 
-		Assert.assertEquals(expectedCalendar.get(Calendar.YEAR), actualCalendar.get(Calendar.YEAR));
-		Assert.assertEquals(expectedCalendar.get(Calendar.MONTH), actualCalendar.get(Calendar.MONTH));
-		Assert.assertEquals(expectedCalendar.get(Calendar.DATE), actualCalendar.get(Calendar.DATE));
-		Assert.assertEquals(expectedCalendar.get(Calendar.HOUR), actualCalendar.get(Calendar.HOUR));
-		Assert.assertEquals(expectedCalendar.get(Calendar.MINUTE), actualCalendar.get(Calendar.MINUTE));
-		Assert.assertEquals(expectedCalendar.get(Calendar.SECOND), actualCalendar.get(Calendar.SECOND));
-	}
+        Assert.assertEquals(expectedCalendar.get(Calendar.YEAR), actualCalendar.get(Calendar.YEAR));
+        Assert.assertEquals(expectedCalendar.get(Calendar.MONTH), actualCalendar.get(Calendar.MONTH));
+        Assert.assertEquals(expectedCalendar.get(Calendar.DATE), actualCalendar.get(Calendar.DATE));
+        Assert.assertEquals(expectedCalendar.get(Calendar.HOUR), actualCalendar.get(Calendar.HOUR));
+        Assert.assertEquals(expectedCalendar.get(Calendar.MINUTE), actualCalendar.get(Calendar.MINUTE));
+        Assert.assertEquals(expectedCalendar.get(Calendar.SECOND), actualCalendar.get(Calendar.SECOND));
+    }
+
+    public static void assertEqual(LocalDateTime expected, LocalDateTime actual) {
+
+        Assert.assertEquals(expected.getYear(), actual.getYear());
+        Assert.assertEquals(expected.getMonth(), actual.getMonth());
+        Assert.assertEquals(expected.getDayOfMonth(), actual.getDayOfMonth());
+        Assert.assertEquals(expected.getHour(), actual.getHour());
+        Assert.assertEquals(expected.getMinute(), actual.getMinute());
+        Assert.assertEquals(expected.getSecond(), actual.getSecond());
+    }
 }
